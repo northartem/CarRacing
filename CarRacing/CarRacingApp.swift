66fec9b1ae28d340c7628826b0e7ec78165ea337
AssanCred <temix@live.ru>
2023-06-27 12:33:31 +0700

    Initial Commit

---
+++ CarRacing/CarRacingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarRacingApp.swift
+//  CarRacing
+//
+//  Created by Артём Латушкин on 27.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CarRacingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
